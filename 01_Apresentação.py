f4f49a6f87e45c6d77ee49ecfec2694d1692196c
ferroeduarda <111460389+ferroeduarda@users.noreply.github.com>
2022-11-05 14:53:10 -0300

    Add files via upload

---
+++ 01_Apresentação.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle 
+import pandas as pd
+
+st.set_page_config(page_title="modeloChikungunya")
+
+st.title('Seja bem vindo!')
+st.header('Olá, sou CHIKV Helper!')
+st.write('Fui desenvolvido para auxiliar no diagnóstico de Chikungunya.')
+st.write('Nessa plataforma, basta informar seus sintomas e retornarei a probabilidade de estar ou não com a doença. Fui elaborado para ser uma ferramenta auxiliar ao diagnóstico de Chikungunya de uma forma rápida e acessível. Aqui, você pode conhecer um pouco mais sobre a doença e como fui desenvolvido!')
+
+
+
