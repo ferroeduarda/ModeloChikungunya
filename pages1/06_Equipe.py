e79c7b91ffd64e638ba06a7fa3c75fdc28bfd0c8
ferroeduarda <111460389+ferroeduarda@users.noreply.github.com>
2022-11-04 22:42:33 -0300

    Add files via upload

---
+++ pages1/06_Equipe.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pickle 
+import pandas as pd
+
+st.title('Equipe Dotlab Brazil')
+st.write('Patrícia Takako Endo \n e-mail: patricia.endo@upe.br')
+st.write('Maria Eduarda Ferro de Mello \n e-mail: ferroeduarda23@gmail.com')
+st.write('Sebastião Rogerio da Silva Neto \n e-mail: srsn@ecomp.poli.br')
+st.write('Kayo Henrique de Carvalho Monteiro \n e-mail: kayohcm@gmail.com')
+st.write('Thomás Tabosa de Oliveira \n e-mail: thomastabosa10@gmail.com')
+st.write('Leonides Medeiros Neto \n e-mail:')
+st.write('Morgana Thalita da Silva Leite \n e-mail:')
